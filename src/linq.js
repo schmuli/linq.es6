83d748938e923a5f2a8bad8ac3c9b0e9a96b576d
Schmuli Raskin <Schmuli Raskin>
2015-01-16 00:27:47 +0200

    Added more functions and refactored existing
    Added:
    take, takeWhile, skip, skipWhile, last, reverse, defaultIfEmpty
    Refactored:
    toArray

--- src/linq.js
+++ src/linq.js
@@ -60,7 +60,7 @@ var Enumerable = (function () {
 
             return this.reduce(function (count) { return count + 1; }, 0);
         }
-
+        
         first(predicate) {
             if (predicate) {
                 return this.filter(predicate).first();
@@ -71,7 +71,26 @@ var Enumerable = (function () {
                 ? next.value.item
                 : undefined;
         }
+        
+        forEach(fn) {
+            for (var i of this.iterator()) {
+                fn(i.item, i.index);
+            }
+            return this;
+        }
 
+        last(predicate) {
+            if (predicate) {
+                return this.filter(predicate).last();
+            }
+            
+            var last;
+            for(var i of this.iterator()) {
+                last = i.item;
+            }
+            return last;
+        }
+        
         single(predicate) {
             if (predicate) {
                 return this.filter(predicate).single();
@@ -122,19 +141,8 @@ var Enumerable = (function () {
             return accumulate;
         }
 
-        forEach(fn) {
-            for (var i of this.iterator()) {
-                fn(i.item, i.index);
-            }
-            return this;
-        }
-
         toArray() {
-            var results = [];
-            this.forEach(function (i) {
-                results.push(i);
-            });
-            return results;
+            return reduceInternal(this, false);
         }
 
         // Lazy
@@ -147,6 +155,33 @@ var Enumerable = (function () {
                 .selectMany(function (x) { return x; });
         }
 
+        defaultIfEmpty(defaultValue) {
+            var iterator = this.iterator;
+            
+            return new Enumerable(function* () {
+                var iter = iterator();
+                
+                var next = iter.next();
+                if (!next.done) {
+                    do {
+                        yield next.value;
+                        next = iter.next();
+                    } while(!next.done);
+                } else {
+                    yield { item: defaultValue, index: 0 };
+                }
+            });
+        }
+        
+        reverse() {
+            var source = this;
+            
+            return new Enumerable(function* () {
+                var reversed = reduceInternal(source, true);                
+                yield* reversed.asEnumerable().iterator();
+            });
+        }
+        
         select(selector) {
             var iterator = this.iterator;
             return new Enumerable(function* () {
@@ -173,6 +208,35 @@ var Enumerable = (function () {
             });
         }
 
+        skip(count) {
+            return this.skipWhile(function (item, index) {
+                return index < count;
+            });
+        }
+        
+        skipWhile(selector) {
+            var iterator = this.iterator;
+            var test = skip;
+            
+            return new Enumerable(function* () {
+                for(var i of iterator()) {
+                    if(!test(i.item, i.index)) {
+                        yield i;
+                    }
+                }
+            });
+            
+            function skip(item, index) {
+                var result = selector(item, index);
+                if (!result) {
+                    test = falseFn;
+                }
+                return result;
+            }
+            
+            function falseFn() { return false; }
+        }
+        
         where(predicate) {
             var iterator = this.iterator;
             return new Enumerable(function* () {
@@ -180,6 +244,24 @@ var Enumerable = (function () {
                     if (predicate(i.item, i.index)) {
                         yield i;
                     }
+                }
+            });
+        }
+        
+        take(count) {
+            return this.takeWhile(function (item, index) {
+                return index < count;
+            });
+        }
+        
+        takeWhile(selector) {
+            var iterator = this.iterator;
+            return new Enumerable(function* () {
+                for(var i of iterator()) {
+                    if (!selector(i.item, i.index)) {
+                        break;
+                    }
+                    yield i;
                 }
             });
         }
@@ -208,6 +290,14 @@ var Enumerable = (function () {
 
     return Enumerable;
 
+    function reduceInternal(enumerable, reverse) {
+        var fn = reverse ? 'unshift' : 'push'
+        return enumerable.reduce(function (result, item) {
+            result[fn](item);
+            return result;
+        }, []);
+    }
+    
     function copy(existingName, newName) {
         Enumerable.prototype[newName] = Enumerable.prototype[existingName];
     }
