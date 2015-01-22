22617a685c8fbbd0bef419a8059a9daf747ce0d6
Schmulik Raskin <schmuli@gmail.com>
2015-01-22 17:55:22 +0200

    Added tests for forEach

--- tests/forEach.js
+++ tests/forEach.js
@@ -1,1 +1,43 @@
+"use strict";
 
+describe('Enumerable.prototype.forEach', function () {
+
+    it('should throw for invalid parameters', function () {
+        // Arrange
+        var enumerable = [1,2,3,4].asEnumerable();
+
+        // Assert
+        expect(function () {
+            enumerable.forEach(null);
+        }).toThrow();
+    });
+
+    it('should return the original enumerable', function () {
+        // Arrange
+        var enumerable = [1,2,3,4].asEnumerable();
+
+        // Act
+        var result = enumerable.forEach(function () {});
+
+        // Assert
+        expect(result).toBe(enumerable);
+    });
+
+    it('should invoke callback for each item', function () {
+        // Arrange
+        var enumerable = [1,2,3,4,5].asEnumerable();
+        var spy = {
+            callback: function () {}
+        };
+        spyOn(spy, 'callback');
+
+        // Act
+        enumerable.forEach(function () {
+            spy.callback();
+        });
+
+        // Assert
+        expect(spy.callback.calls.count()).toBe(5);
+    });
+
+});
