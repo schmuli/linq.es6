07b68ec1fb3297b74848b281d8ee28868f562a88
Schmulik Raskin <schmuli@gmail.com>
2015-01-22 18:17:19 +0200

    Added tests for map/select

--- tests/map.js
+++ tests/map.js
@@ -1,1 +1,41 @@
+"use strict";
 
+describe('Enumerable.prototype.map', function () {
+
+    it('should throw when passed invalid predicate', function () {
+        // Arrange
+        var enumerable = Enumerable.range(0, 4);
+
+        // Assert
+        expect(function () {
+            enumerable.map(null);
+        }).toThrow();
+    });
+
+    it('should return empty enumerable for an empty enumerable', function () {
+        // Arrange
+        var enumerable = Enumerable.empty();
+
+        // Act
+        var result = enumerable.map(function () {});
+
+        // Assert
+        expect(result.toArray().length).toBe(0);
+    });
+
+    it('should returned mapped value for each element', function () {
+        // Arrange
+        var enumerable = Enumerable.range(1, 5);
+
+        // Act
+        var result = enumerable.map(function (i) {
+            return i + ' mapped';
+        });
+
+        // Assert
+        result.toArray().forEach(function (i, index) {
+            expect(i).toBe((index + 1) + ' mapped');
+        });
+    });
+
+});
